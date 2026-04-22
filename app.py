b01240caf29f7a2b97811da0d198620be876070c
HARSHAKOLIPAKA <harshavardhankolipaka5@gmail.com>
2026-04-22 22:49:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Bill App Running Successfully"
+    return render_template("index.html")
+
+@app.route("/generate", methods=["POST"])
+def generate():
+    name = request.form["name"]
+    amount = request.form["amount"]
+    service = request.form["service"]
+
+    return f"""
+    <h2>Bill Generated</h2>
+    <p>Customer: {name}</p>
+    <p>Service: {service}</p>
+    <p>Amount: ₹{amount}</p>
+    """
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
