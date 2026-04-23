aeab876681f036642701650b774d80cc03c608b6
HARSHAKOLIPAKA <harshavardhankolipaka5@gmail.com>
2026-04-23 09:47:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-bill_no = 1
 from flask import Flask, render_template, request
+
 app = Flask(__name__)
+
+# 🔢 Bill counter
+bill_no = 1
 
 @app.route("/")
 def home():
@@ -8,9 +11,20 @@ def home():
 
 @app.route("/bill", methods=["POST"])
 def bill():
+    global bill_no  # important
+
     customer = request.form.get("customer")
     amount = request.form.get("amount")
-    return render_template("bill.html", customer=customer, amount=amount)
+
+    current_bill = bill_no
+    bill_no += 1  # increase for next bill
+
+    return render_template(
+        "bill.html",
+        customer=customer,
+        amount=amount,
+        bill_no=current_bill
+    )
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
