f493f89b38091075723fa3fa0040a8364024d62a
HARSHAKOLIPAKA <harshavardhankolipaka5@gmail.com>
2026-04-23 09:39:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,18 +2,15 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-# HOME PAGE (this is what opens on your link)
 @app.route("/")
 def home():
     return render_template("index.html")
 
-# BILL SUBMIT PAGE
 @app.route("/bill", methods=["POST"])
 def bill():
     customer = request.form.get("customer")
     amount = request.form.get("amount")
-    return f"Bill created for {customer} - ₹{amount}"
+    return render_template("bill.html", customer=customer, amount=amount)
 
-# IMPORTANT for Render
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
